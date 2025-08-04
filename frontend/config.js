71642eb34154952ffae6b632cecf9a35b5c3675a
Rajansinh Chudasama <rajan.chudasama@brainvire.com>
2025-08-04 17:42:44 +0530

    #main: Downloaded changes that were done directly on production server

--- frontend/config.js
+++ frontend/config.js
@@ -2,10 +2,13 @@
 //For environment variables 
 
 const CONFIG = {
-    API_REC_URL: 'http://localhost:8080/api/rec/',
-    API_MED_URL:'http://localhost:8080/api/media',
-    API_URL: 'http://localhost:8080/api',
+    // API_REC_URL: 'http://localhost:8080/api/rec/',
+    // API_MED_URL:'http://localhost:8080/api/media',
+    // API_URL: 'http://localhost:8080/api',
     // API_REC_URL: 'http://192.168.100.191:8080/api/rec/',
     // API_MED_URL:'http://192.168.100.191:8080/api/media',
     // API_URL: 'http://192.168.100.191:8080/api',
+    API_REC_URL: 'https://api.tuneup.golf/api/rec/',
+    API_MED_URL: 'https://api.tuneup.golf/api/media',
+    API_URL: 'https://api.tuneup.golf/api',
 };
